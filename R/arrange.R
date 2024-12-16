58e28a8502bff337410d7615fe89902d1f0f2bc1
MikeJohnPage <38110953+MikeJohnPage@users.noreply.github.com>
2024-12-16 16:17:29 +0000

    Add arrange block

---
+++ R/arrange.R
@@ -0,0 +1,59 @@
+#' Arrange block constructor
+#'
+#' This block allows allows you to order the rows of a data frame by the values
+#' of selected columns (see [dplyr::arrange()]).
+#'
+#' @param columns Columns to arrange by.
+#' @param ... Forwarded to [new_block()]
+#'
+#' @export
+new_arrange_block <- function(columns = character(), ...) {
+  new_transform_block(
+    function() {
+      server <- moduleServer(
+        "arrange_expr",
+        function(input, output, server) {
+          arrange_by <- reactiveVal(columns)
+          choices <- reactive(colnames(data()))
+
+          arrange_by(input$columns) |>
+            bindEvent(input$columns)
+
+          observe(
+            updateSelectInput(
+              session,
+              inputId = "columns",
+              choices = choices(),
+              selected = arrange_by()
+            )
+          )
+
+          list(
+            expr = reactive(
+              bquote(
+                dplyr::arrange(data, ..(vars)),
+                list(vars = lapply(arrange_by(), as.name)),
+                splice = TRUE
+              )
+            ),
+            state = list(
+              columns = reactive(arrange_by()),
+              choices = reactive(choices())
+            )
+          )
+        }
+      )
+    },
+    ui = function(ns, columns, choices = character()) {
+      selectInput(
+        ns("arrange_expr", "dataset"),
+        label = "Arrange by:",
+        choices = choices,
+        selected = columns,
+        multiple = TRUE
+      )
+    },
+    class = "arrange_block",
+    ...
+  )
+}
