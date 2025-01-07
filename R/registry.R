6173c075fa89f9f4ec78ea3eb3d7000e4c990560
Nicolas Bennett <3158446+nbenn@users.noreply.github.com>
2025-01-07 11:22:11 +0100

    fix registration

--- R/registry.R
+++ R/registry.R
@@ -1,8 +1,8 @@
 register_dplyr_blocks <- function() {
   register_blocks(
-    constructor = c(
-      new_select_block,
-      new_join_block
+    c(
+      "new_select_block",
+      "new_join_block"
     ),
     name = c(
       "select block",
@@ -16,6 +16,7 @@ register_dplyr_blocks <- function() {
       "transform",
       "transform"
     ),
+    package = utils::packageName(),
     overwrite = TRUE
   )
 }
