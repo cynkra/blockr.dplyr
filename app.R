e9a0030c2829d706f692c380869a30204e0cb609
karmatarap <dorjetarap@gmail.com>
2025-01-09 16:21:23 +0100

    add example

--- app.R
+++ app.R
@@ -10,3 +10,5 @@ blockr.core::serve(new_filter_block(), list(data = mtcars))
 
 blockr.core::serve(new_llm_transform_block(), list(data1 = mtcars, data2 = iris))
 
+# try as a question:
+# "summarize data2 by species and provide, n, mean, min, max for all numeric variables"
