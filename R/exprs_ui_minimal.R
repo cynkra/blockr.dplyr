3dde1c42d7f9312985bb118debd594965f3da6cb
Christoph Sax <christoph.sax@gmail.com>
2025-01-20 12:52:37 +0100

    custom auto completer

--- R/exprs_ui_minimal.R
+++ R/exprs_ui_minimal.R
@@ -2,7 +2,6 @@
 #'
 #' @param id Character string, an identifier for the UI element
 #' @param value Default value for the expression
-#' @param auto_complete_list List of autocompletion options
 #' @return A div element containing the UI components
 #' @importFrom shinyAce aceEditor
 #' @importFrom shiny div
@@ -13,9 +12,66 @@
 #' }
 exprs_ui_minimal <- function(
   id = "",
-  value = NULL,
-  auto_complete_list = NULL
+  value = ""
 ) {
+  # Custom completer script
+  custom_completer <- sprintf('
+    var customCompleter = {
+      getCompletions: function(editor, session, pos, prefix, callback) {
+        // Define categories and their functions
+        var categories = {
+          arithmetic: ["abs", "sign", "ceiling", "floor", "round", "trunc",
+                      "log", "log2", "log10", "exp", "sqrt"],
+          aggregate: ["mean", "sum", "min", "max"],
+          offset: ["lead", "lag", "cumsum", "cumprod", "cummin", "cummax"],
+          logical: ["if_else", "case_when"],
+          string: ["str_c", "paste", "paste0", "str_sub", "str_to_lower", "str_to_upper"],
+          ranking: ["row_number", "min_rank", "dense_rank", "percent_rank", "ntile"]
+        };
+
+        var wordList = [];
+        Object.keys(categories).forEach(function(category) {
+          categories[category].forEach(function(fn) {
+            wordList.push({
+              caption: fn,
+              snippet: fn + "()",
+              value: fn,
+              score: 1000,
+              meta: category
+            });
+          });
+        });
+
+        // Sort by category and then by function name
+        wordList.sort(function(a, b) {
+          if (a.meta === b.meta) {
+            return a.caption.localeCompare(b.caption);
+          }
+          return a.meta.localeCompare(b.meta);
+        });
+        callback(null, wordList);
+      }
+    };
+
+    // Add custom key handler for tab
+    document.addEventListener("DOMContentLoaded", function() {
+      setTimeout(function() {
+        var editor = ace.edit("expr");
+        editor.commands.on("afterExec", function(e) {
+          if (e.command.name == "insertstring" || e.command.name == "insertMatch") {
+            var pos = editor.getCursorPosition();
+            var line = editor.session.getLine(pos.row);
+            var prefix = line.substring(0, pos.column);
+            if (prefix.match(/\\w+\\($/)) {
+              editor.moveCursorTo(pos.row, pos.column - 1);
+            }
+          }
+        });
+      }, 100);
+    });
+
+    ace.require("ace/ext/language_tools").addCompleter(customCompleter);
+  ')
 
   tagList(
     tags$style(".mutate-expression .shiny-ace {
@@ -27,6 +83,8 @@ exprs_ui_minimal <- function(
     .mutate-expression .mutate-code {
       width: 100%;
     }"),
+    # Initialize completer immediately after Ace is loaded
+    tags$script(HTML(custom_completer)),
     div(
       class = paste(
         "input-group mb-3",
@@ -39,8 +97,7 @@ exprs_ui_minimal <- function(
           value = value,
           mode = "r",
           autoComplete = "live",
-          autoCompleters = c("rlang", "static"),
-          autoCompleteList = auto_complete_list,
+          autoCompleters = c("custom"),
           height = "20px",
           showPrintMargin = FALSE,
           highlightActiveLine = FALSE,
@@ -71,21 +128,32 @@ run_minimal_example <- function() {
   shinyApp(
     ui = bslib::page_fluid(
       theme = bslib::bs_theme(version = 5),
+      shinyjs::useShinyjs(),
       div(
         class = "container mt-3",
         exprs_ui_minimal(
           id = "expr",
-          value = "x, y",
-          auto_complete_list = list(data = colnames(df))
+          value = ""
         ),
         verbatimTextOutput("value")
       )
     ),
     server = function(input, output, session) {
-      shinyAce::aceAutocomplete("expr_val")
+      # Initialize ace editor
+      observe({
+        shinyjs::runjs("
+          if (typeof ace !== 'undefined') {
+            var editor = ace.edit('expr');
+            editor.setOptions({
+              enableLiveAutocompletion: true,
+              enableBasicAutocompletion: true
+            });
+          }
+        ")
+      })
 
       output$value <- renderPrint({
-        input$expr_val
+        input$expr
       })
     }
   )
