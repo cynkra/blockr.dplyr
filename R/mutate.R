65b8920c46813fb938d905ee68d24056f81e7577
Christoph Sax <christoph.sax@gmail.com>
2025-01-21 06:32:15 +0100

    update string on submit only

--- R/mutate.R
+++ R/mutate.R
@@ -3,7 +3,7 @@
 #' This block allows to add new variables and preserve existing ones
 #' (see [dplyr::mutate()]). Changes are applied after clicking the submit button.
 #'
-#' @param r_strings Reactive expression returning character vector of
+#' @param string Reactive expression returning character vector of
 #'   expressions
 #' @param ... Additional arguments forwarded to [new_block()]
 #'
@@ -22,7 +22,7 @@
 #' serve(new_mutate_block(), list(data = df))
 #' }
 #' @export
-new_mutate_block <- function(r_strings, ...) {
+new_mutate_block <- function(string = character(), ...) {
   new_transform_block(
     function(data) {
       moduleServer(
@@ -32,33 +32,42 @@ new_mutate_block <- function(r_strings, ...) {
             colnames(data())
           })
 
-          r_ans <- mod_kvexpr_server(
+          r_string <- mod_kvexpr_server(
             id = "kv",
             get_value = \() c(newcol = 'paste("my", "expression")'),
             get_cols = \() colnames(data())
           )
 
           # Store the validated expression
-          r_validated <- reactiveVal(parse_mutate())
+          r_expr_validated <- reactiveVal(parse_mutate())
+          r_string_validated <- reactiveVal(string)
 
           # Validate and update on submit
           observeEvent(input$submit, {
-            strings <- r_ans()
+            string <- r_string()
 
             # If empty or only whitespace, return simple mutate
-            if (all(trimws(unname(strings)) == "")) {
-              expr <- parse_mutate(strings)
-              r_validated(expr)
+            if (all(trimws(unname(string)) == "")) {
+              expr <- parse_mutate(string)
+              r_expr_validated(expr)
               return()
             }
 
-            req(strings)
-            stopifnot(is.character(strings), !is.null(names(strings)))
+            req(string)
+            stopifnot(is.character(string), !is.null(names(string)))
 
-            mutate_string <- glue::glue("{names(strings)} = {unname(strings)}")
-            expr <- parse_mutate(mutate_string)
-
+            mutate_string <- glue::glue("{names(string)} = {unname(string)}")
+            expr <- try(parse_mutate(mutate_string))
             # Validation
+            if (inherits(expr, "try-error")) {
+              showNotification(
+                expr,
+                type = "error",
+                duration = 5
+              )
+              return()
+            }
+            print(expr)
             data <- data()
             ans <- try(eval(expr))
             if (inherits(ans, "try-error")) {
@@ -69,13 +78,19 @@ new_mutate_block <- function(r_strings, ...) {
               )
               return()
             }
-            r_validated(expr)
+            r_expr_validated(expr)
+            r_string_validated(r_string())
+          })
+
+          observe({
+            print(r_expr_validated())
+            print(r_string_validated())
           })
 
           list(
-            expr = r_validated,
+            expr = r_expr_validated,
             state = list(
-              r_strings = r_ans
+              string = r_string_validated
             )
           )
         }
